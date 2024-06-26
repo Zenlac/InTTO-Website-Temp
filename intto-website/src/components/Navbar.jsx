e915e891f77d23aeebe691c5fe590cb735486bce
Zenlac <caleleizengrillera@gmail.com>
2024-06-26 21:39:18 +0800

    finally working

--- intto-website/src/components/Navbar.jsx
+++ intto-website/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 const Navbar = () => {
   return (
-    <div className='text-white'>
-        <h1 className='text-orange-500'>InTTO</h1>
+    <div className='flex justify-between items-center h-24 mx-auto px-4 text-white'>
+        <h1 className='w-full text-3xl font-bold'>InTTO</h1>
+        <ul className='flex'>
+          <li className='p-4'>Home</li>
+          <li className='p-4'>About</li>
+          <li className='p-4'>Services</li>
+          <li className='p-4'>Startups</li>
+          <li className='p-4'>Events</li>
+        </ul>
     </div>
   )
 }
