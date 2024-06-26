aa4dbd43aa5565c907d2c894890649ac041bf44e
Zenlac <caleleizengrillera@gmail.com>
2024-06-26 16:28:25 +0800

    atleast not times new roman

---
+++ intto-website/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+        <h1 className='text-orange-500'>InTTO</h1>
+    </div>
+  )
+}
+
+export default Navbar
