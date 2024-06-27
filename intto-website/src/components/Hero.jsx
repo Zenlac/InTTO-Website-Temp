3a201e19e6ca1a2a06a54aa2c3ea1636d8c44dee
Zenlac <caleleizengrillera@gmail.com>
2024-06-27 15:23:11 +0800

    started with homepage

---
+++ intto-website/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+        <div className='max-w-[350px] mt-[96px] w-full h-screen mx-auto text-center flex-col justify-center'>
+            <h1 className='font-semibold text-4xl'>Driving the Culture of Innovation</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- intto-website/src/components/Navbar.jsx
+++ intto-website/src/components/Navbar.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { IoIosMenu,IoIosClose } from "react-icons/io";
+import logo from './images/InTTO Logo White.svg';
 const Navbar = () => {
+  const[nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
   return (
-    <div className='flex justify-between items-center h-24 mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold'>InTTO</h1>
-        <ul className='flex'>
+    <div className='flex justify-center items-center h-16 mx-auto px-4 text-white'>
+        <img src={logo} alt="Logo" className='object-scale-down h-8 w-20 fixed left-4 top-4'/> 
+        <ul className='hidden md:flex'>
           <li className='p-4'>Home</li>
           <li className='p-4'>About</li>
           <li className='p-4'>Services</li>
           <li className='p-4'>Startups</li>
           <li className='p-4'>Events</li>
         </ul>
+
+        <div onClick={handleNav} className='fixed right-4 top-4 block md:hidden'>
+          {!nav ? <IoIosClose size={20}/> : <IoIosMenu size={20}/>}
+        </div>
+
+        <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#114331] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+          <img src={logo} alt="Logo" className='object-scale-down h-8 w-124 m-4 '/>
+          <ul className='p-4 pt-8 uppercase my-8'>
+            <li className='p-4 border-b border-gray-300'>Home</li>
+            <li className='p-4 border-b border-gray-300'>About</li>
+            <li className='p-4 border-b border-gray-300'>Services</li>
+            <li className='p-4 border-b border-gray-300'>Startups</li>
+            <li className='p-4'>Events</li>
+          </ul>
+        </div>
     </div>
+    
   )
 }
 
